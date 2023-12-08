5ac20e1caab0c8024f72dc26f98d0597a015c5ca
Destiny <desto4q@gmail.com>
2023-12-09 00:30:37 +0100

    first commit

---
+++ scripts/data.js
@@ -0,0 +1,22 @@
+let links = [
+    {
+        to: "#",
+        name: "About"
+    },
+    {
+        to: "#",
+        name: "Contact"
+    },
+    {
+        to: "#",
+        name: "FAQ"
+    },
+    {
+        to: "#",
+        name: ""
+    },
+    {
+        to: "#",
+        name: ""
+    },
+]
---
+++ scripts/index.js
@@ -0,0 +1,60 @@
+let nav_bar = document.querySelector(".nav_links")
+let side_nav = document.querySelector(".side_nav")
+let nav_btn = document.querySelector(".nav_btn")
+
+
+let div_maker = (type, content) => {
+    let div = document.createElement(`${type}`)
+    div.innerHTML = content
+    return div
+}
+
+for (let i in links) {
+    let a = div_maker("a", links[i].name)
+    a.setAttribute("href", "#")
+    nav_bar.append(a)
+}
+
+
+let side_nav_activator = () => {
+    let shown = false
+    return {
+        switcher: () => {
+            if (shown == false) {
+                side_nav.classList.add("open")
+                shown = true
+                nav_btn.classList.add("checked")
+            }
+            else {
+                side_nav.classList.remove("open")
+                shown = false
+                nav_btn.classList.remove("checked")
+            }
+        },
+        nav_close: () => {
+            if (shown != false) {
+                side_nav.classList.remove("open")
+                shown = false
+                nav_btn.classList.remove("checked")
+            }
+        }
+    }
+
+}
+
+let nav_activate = side_nav_activator()
+
+
+nav_btn.onmousedown = () => {
+    nav_activate.switcher()
+}
+
+
+console.log(nav_bar)
+
+window.onresize = () => {
+
+    if (window.innerWidth > 600) {
+        nav_activate.nav_close()
+    }
+}
